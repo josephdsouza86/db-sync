a58e7bbe5070dd2bf485beffbcb7dea29eef1dba
Joseph D'Souza <joseph.dsouza@alphalabs.net>
2022-03-02 12:21:44 +0000

    Fix directory paths with spaces

--- database-update.sh
+++ database-update.sh
@@ -31,10 +31,10 @@ REMOTE_DATABASE_PASS=$REMOTE_DB_PASSWORD
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 
 # Create directory to store dumps
-mkdir -p $CONFIG/dumps
+mkdir -p "$CONFIG/dumps"
 
 echo Backing up local database: $LOCAL_DATABASE_NAME
-mysqldump -v -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME > $CONFIG/dumps/local-database-$CURRENT_TIME.sql
+mysqldump -v -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME > "$CONFIG/dumps/local-database-$CURRENT_TIME.sql"
 
 # Delete local database
 echo Dropping local database: $LOCAL_DATABASE_NAME
@@ -49,16 +49,17 @@ if mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS 
 
 	# Download database dump
 	echo Exporting database \'$REMOTE_DATABASE_NAME\' from remote server: $REMOTE_DATABASE_HOST
-	mysqldump -v -h $REMOTE_DATABASE_HOST -u $REMOTE_DATABASE_USER -p$REMOTE_DATABASE_PASS $REMOTE_DATABASE_NAME > $CONFIG/dumps/remote-database-$CURRENT_TIME.sql
+	mysqldump -v -h $REMOTE_DATABASE_HOST -u $REMOTE_DATABASE_USER -p$REMOTE_DATABASE_PASS $REMOTE_DATABASE_NAME > "$CONFIG/dumps/remote-database-$CURRENT_TIME.sql"
 
 	echo Remote database exported: remote-database-$CURRENT_TIME.sql
 
 	# Upload dump to local database
 	echo Importing database \'remote-database-$CURRENT_TIME.sql\' to local server: $LOCAL_DATABASE_HOST
-	mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME < $CONFIG/dumps/remote-database-$CURRENT_TIME.sql
+	mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME < "$CONFIG/dumps/remote-database-$CURRENT_TIME.sql"
 
 	echo COMPLETE: Database update complete: $LOCAL_DATABASE_NAME
 
 else
 	echo ERROR: Could not connect to the local or remote database
+	read -p "Press enter to continue"
 fi
